23c6c5fd440f565c1893db3c3be72471f12bddfa
Kac1509 <kaleem.corbin@gmail.com>
2018-12-17 00:13:10 +0100

    Create CSV Function

--- Classifier/scripts/predictClass.py
+++ Classifier/scripts/predictClass.py
@@ -9,7 +9,7 @@ def predictClass(model,prediction_path, shapeY, shapeX, class_mode='binary'):
         prediction_path,
         target_size=(shapeY, shapeX),
         batch_size=1,
+        shuffle = False,
         class_mode=class_mode)
     predictions = model.predict_generator(test_generator, verbose=1)
-    print(test_generator.filenames)
-    return predictions
+    return test_generator,predictions
